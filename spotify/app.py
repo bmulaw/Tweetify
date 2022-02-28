23a2e28c889fd21f6a346268ea332b4d8bed8c2a
Bernard Mulaw <bernard@nat-wireless-guest-reg-153-50.bu.edu>
2022-02-28 03:29:35 -0500

    adding spotipy to run simple python script on spotify api

---
+++ spotify/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from decouple import config
+import spotipy
+import spotipy.util as util
+
+app = Flask(__name__)
+
+client_id = config('CLIENT_ID')
+client_secret = config('CLIENT_SECRET')
+
+token = util.prompt_for_user_token(
+             username,
+             scope,
+             client_id=client_id, 
+             client_secret=client_secret,
+             redirect_uri='http://localhost:5000/') 
+sp = spotipy.Spotify(auth=token)
+print(sp)
+@app.route('/')
+def test():
+    return "this is a test on spotify.py"
+
+if __name__ == "__main__":
+    app.run(debug=True)
